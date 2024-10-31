15fce8f0235a0bd4093d2b699b82fe9334543354
aydpinto <contactaydenpinto@gmail.com>
2024-10-30 23:12:22 -0400

    website frame

--- global.js
+++ global.js
@@ -1,48 +1,31 @@
+// Modal Functionality
+window.onload = function() {
+  var modal = document.getElementById('welcome-modal');
+  var closeBtn = document.getElementsByClassName('close-btn')[0];
 
-// JS for the navbar.
-// Everytime you scroll below a certain amount, a shadow is added to the navbar.
-window.onscroll = function () { scrollFunction() };
+  // Display the modal after 2 seconds
+  setTimeout(function() {
+      modal.style.display = 'block';
+  }, 2000);
 
-function scrollFunction() {
-  if (document.body.scrollTop > 40 || document.documentElement.scrollTop > 40) {
-    document.getElementById("navbar-extra").style.boxShadow = "0px 3px 3px rgba(0, 0, 0, 0.102)";
-  } else {
-    document.getElementById("navbar-extra").style.boxShadow = "none";
+  // Close the modal when the user clicks on <span> (x)
+  closeBtn.onclick = function() {
+      modal.style.display = 'none';
   }
-}
 
-// Blog modal JS
-// Opens up Blog with an animation
-document.addEventListener("shown.bs.modal", function (event) {
-  const modalBodyElements = event.target.querySelectorAll(".modal-body > *");
-  let delay = 0.2;
-
-  modalBodyElements.forEach(element => {
-    element.style.animationDelay = `${delay}s`;
-    delay += 0.2;
-  });
-});
-
-document.addEventListener("hidden.bs.modal", function (event) {
-  const modalBodyElements = event.target.querySelectorAll(".modal-body > *");
-
-  modalBodyElements.forEach(element => {
-    element.style.opacity = "0";
-    element.style.transform = "translateY(10px)";
-  });
-});
-
-// Front page modal JS
-function openModal(modalId) {
-  document.getElementById(modalId).style.display = "flex";
-}
-
-function closeModal(modalId) {
-  document.getElementById(modalId).style.display = "none";
-}
-
-window.onclick = function (event) {
-  if (event.target.classList.contains('modal')) {
-      event.target.style.display = "none";
+  // Close the modal when the user clicks anywhere outside of it
+  window.onclick = function(event) {
+      if (event.target == modal) {
+          modal.style.display = 'none';
+      }
   }
+
+  // Burger Menu Toggle
+  var burger = document.querySelector('.burger');
+  var navLinks = document.querySelector('.nav-links');
+
+  burger.addEventListener('click', () => {
+      navLinks.classList.toggle('nav-active');
+      burger.classList.toggle('toggle');
+  });
 }
