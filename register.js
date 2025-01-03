1f69fcb1b0205ec9c9f285aa12f2ea92e9de0697
MPuri007 <medhpuri@gmail.com>
2025-01-03 13:51:16 -0500

    fixes

--- register.js
+++ register.js
@@ -1,5 +1,5 @@
 // Import the functions you need from the SDKs you need
-import { initializeApp } from "https://www.gstatic.com/firebasejs/11.1.0/firebase-app.js";
+import { initializeApp } from "https://www.gstatic.com/firebasejs/11.0.2/firebase-app.js";
 import {
   getAuth,
   createUserWithEmailAndPassword
